f18c7b3f6beb1b7ab63204041a14cbeca944aeb6
paradox7183 <41882875+paradox7183@users.noreply.github.com>
2018-08-03 15:38:46 -0500

    Add files via upload

---
+++ zeroscript.sh
@@ -0,0 +1,45 @@
+
+mkdir Scriptzero
+sleep 5
+cd Scriptzero
+clear
+sl
+setterm -foreground cyan
+figlet Zer0Script
+echo "Made By Zero"
+apt update && apt upgrade 
+pkg update && pkg upgrade 
+pkg install python3 
+pkg install python 
+pkg install python2 
+figlet Routersploit 
+git clone https://github.com/threat9/routersploit
+echo "Install Requirements.txt & Requirements-dev.txt"
+setterm -foreground green
+figlet ReconDog 
+git clone https://github.com/s0md3v/ReconDog
+sleep 5
+echo "ReconDog Installed"
+setterm -foreground red
+figlet SQLmap
+git clone https://github.com/sqlmapproject/sqlmap
+sleep 6 
+echo "SQLmap Installed!"
+sleep 2
+setterm -foreground blue
+figlet Hakku 
+git clone https://github.com/4shadoww/hakkuframework
+echo "Hakku Installed"
+setterm -foreground yellow 
+sleep 5 
+figlet Ip
+figlet Geo 
+figlet localizator 
+git clone https://github.com/maldevel/IPGeoLocation 
+echo "Installed Requeriments.txt"
+figlet Made
+figlet By 
+figlet Zer0
+echo "Thanks for use my script ;)" 
+echo "See you later"
+
